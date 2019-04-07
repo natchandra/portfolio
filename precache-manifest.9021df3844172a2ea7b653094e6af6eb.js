705ddbf89d740cbdff1c9ae590e7063c2df4ef34
aswinwibisurya <aswinwibisurya@gmail.com>
2019-04-07 20:11:46 +0700

    Updates

---
+++ precache-manifest.9021df3844172a2ea7b653094e6af6eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6515f03fe380ac1fe856b9267e4e2138",
+    "url": "/portfolio/static/media/girl_avatar.6515f03f.jpg"
+  },
+  {
+    "revision": "c42ac41f07c491e490942ca971309423",
+    "url": "/portfolio/static/media/background.c42ac41f.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4e4e9b0b68121ffb187b",
+    "url": "/portfolio/static/js/main.75341e67.chunk.js"
+  },
+  {
+    "revision": "3a4b766e4e738864bbf3",
+    "url": "/portfolio/static/js/2.015ac134.chunk.js"
+  },
+  {
+    "revision": "4e4e9b0b68121ffb187b",
+    "url": "/portfolio/static/css/main.cbac957c.chunk.css"
+  },
+  {
+    "revision": "d56d23af4079617208f572343b1c0378",
+    "url": "/portfolio/index.html"
+  }
+];
