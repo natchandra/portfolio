725c62fa5fd111b129845698065d25deb6dba532
aswinwibisurya <aswinwibisurya@gmail.com>
2019-04-07 21:38:28 +0700

    Updates

--- precache-manifest.ea6f61aeac611cef8c366241da88226c.js
+++ precache-manifest.ea6f61aeac611cef8c366241da88226c.js
@@ -12,19 +12,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
   },
   {
-    "revision": "cf68426c72338fbf1627",
-    "url": "/portfolio/static/js/main.62811f2d.chunk.js"
+    "revision": "51b8b649cf708153fd1a",
+    "url": "/portfolio/static/js/main.c083ee8a.chunk.js"
   },
   {
     "revision": "3a4b766e4e738864bbf3",
     "url": "/portfolio/static/js/2.015ac134.chunk.js"
   },
   {
-    "revision": "cf68426c72338fbf1627",
-    "url": "/portfolio/static/css/main.cbac957c.chunk.css"
+    "revision": "51b8b649cf708153fd1a",
+    "url": "/portfolio/static/css/main.6159936d.chunk.css"
   },
   {
-    "revision": "b9fc52510e454c6fedf32e65e507ef2f",
+    "revision": "8a15df48c73a812e34d773b3077f8abf",
     "url": "/portfolio/index.html"
   }
 ];
